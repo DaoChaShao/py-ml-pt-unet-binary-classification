64aff0ec7ed9c1c172317b6dd53f37b901476648
DaoChaShao <27882272@qq.com>
2025-11-22 13:46:47 +0800

    fix: correct file name in header comment and clean up unused code in seg_PTV.py

--- src/datasets/seg_PTV.py
+++ src/datasets/seg_PTV.py
@@ -3,7 +3,7 @@
 # @Time     :   2025/11/15 02:40
 # @Author   :   Shawn
 # @Version  :   Version 0.1.0
-# @File     :   seg_PT.py
+# @File     :   seg_PTV.py
 # @Desc     :   
 
 from numpy import array, ndarray, float32, unique
@@ -100,7 +100,6 @@ class PTVDataset(Dataset):
         else:
             image: Tensor = from_numpy(array(image)).permute(2, 0, 1).float() / 255.0
 
-        # mask_map_class_id 是你原来自定义函数
         converted_mask, _ = mask_map_class_id(mask)
         converted_mask: Image.Image = Image.fromarray(converted_mask)
 
@@ -108,9 +107,6 @@ class PTVDataset(Dataset):
             mask: Tensor = self._mask_transformer(converted_mask)
         else:
             mask: Tensor = from_numpy(array(converted_mask)).float()
-
-        # 强化小目标权重：可选，对 Pedestrian 数据集小目标增加权重
-        # mask = mask * 2.0  # 训练时可尝试
 
         return image, mask
 
