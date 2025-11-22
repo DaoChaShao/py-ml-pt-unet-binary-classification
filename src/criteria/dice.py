2d3d6c931bc9fe1ec75415692b91c903aa901341
DaoChaShao <27882272@qq.com>
2025-11-22 13:45:05 +0800

    feat: implement DiceBCELoss class for improved loss calculation

--- src/criteria/dice.py
+++ src/criteria/dice.py
@@ -6,9 +6,30 @@
 # @File     :   dice.py
 # @Desc     :   
 
-def main() -> None:
-    """ Main Function """
-    pass
+from torch import nn, sigmoid
+
+
+class DiceBCELoss(nn.Module):
+    def __init__(self, pos_weight=None, smooth: float = 1e-6):
+        super().__init__()
+        self._pos_weight = pos_weight
+        self._smooth = smooth
+
+    def forward(self, inputs, targets):
+        sig_out = sigmoid(inputs)
+
+        intersection = (sig_out * targets).sum()
+        dice = (2. * intersection + self._smooth) / (sig_out.sum() + targets.sum() + self._smooth)
+        dice_loss = 1 - dice
+
+        bce_loss = nn.functional.binary_cross_entropy_with_logits(
+            inputs, targets,
+            pos_weight=self._pos_weight,
+            reduction="mean"
+        )
+
+        return bce_loss + dice_loss
+
 
 if __name__ == "__main__":
-    main()
+    pass
