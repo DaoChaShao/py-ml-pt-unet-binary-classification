bc589c9574c57afd21dda0480ad1a7f9e1abe30f
DaoChaShao <27882272@qq.com>
2025-11-23 18:58:39 +0800

    refactor: enhance module documentation and update exports in __init__.py

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -3,13 +3,29 @@
 # @Time     :   2025/11/15 00:19
 # @Author   :   Shawn
 # @Version  :   Version 0.1.0
-# @File     :   __init__.py.py
+# @File     :   __init__.py
 # @Desc     :   
 
-def main() -> None:
-    """ Main Function """
-    pass
+"""
+****************************************************************
+Datasets Module
+----------------------------------------------------------------
+Medical image segmentation datasets and utilities.
+- ALBDataset: Albumentations-based segmentation dataset
+- PTVDataset: Generic UNet segmentation dataset
+- mask_map_class_id: Mask to class ID mapping utility
+****************************************************************
+"""
 
+__author__ = "Shawn Yu"
+__version__ = "0.1.0"
 
-if __name__ == "__main__":
-    main()
+from .seg_ALB import ALBDataset
+from .seg_map import mask_map_class_id
+from .seg_PTV import PTVDataset
+
+__all__ = [
+    "ALBDataset",
+    "mask_map_class_id",
+    "PTVDataset",
+]
