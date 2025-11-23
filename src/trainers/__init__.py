097ddfc8335a08af1f54cfe9ffd0fc316a24fb40
DaoChaShao <27882272@qq.com>
2025-11-23 19:02:18 +0800

    refactor: update module documentation and define exports for neural network trainer

--- src/trainers/__init__.py
+++ src/trainers/__init__.py
@@ -3,13 +3,23 @@
 # @Time     :   2025/11/15 00:19
 # @Author   :   Shawn
 # @Version  :   Version 0.1.0
-# @File     :   __init__.py.py
+# @File     :   __init__.py
 # @Desc     :   
 
-def main() -> None:
-    """ Main Function """
-    pass
+"""
+****************************************************************
+Neural Nets Trainer Module
+----------------------------------------------------------------
+Training utilities for neural network models.
+- UNetSegmentationTrainer: Trainer for UNet-based image segmentation tasks
+****************************************************************
+"""
 
+__author__ = "Shawn Yu"
+__version__ = "0.1.0"
 
-if __name__ == "__main__":
-    main()
+from .binary_seg import UNetSegmentationTrainer
+
+__all__ = [
+    "UNetSegmentationTrainer",
+]
