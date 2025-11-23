c1e0cfe0805898c31b30b24ad93aa5e78608428f
DaoChaShao <27882272@qq.com>
2025-11-23 19:02:09 +0800

    refactor: update module documentation and define exports in __init__.py

--- src/nets/__init__.py
+++ src/nets/__init__.py
@@ -3,13 +3,26 @@
 # @Time     :   2025/11/15 00:17
 # @Author   :   Shawn
 # @Version  :   Version 0.1.0
-# @File     :   __init__.py.py
+# @File     :   __init__.py
 # @Desc     :   
 
-def main() -> None:
-    """ Main Function """
-    pass
+"""
+****************************************************************
+Neural Nets Module
+----------------------------------------------------------------
+UNet-based neural network architectures.
+- Standard4LayersUNet: 4-layer UNet variant
+- Standard5LayersUNet: 5-layer UNet variant
+****************************************************************
+"""
 
+__author__ = "Shawn Yu"
+__version__ = "0.1.0"
 
-if __name__ == "__main__":
-    main()
+from .standard4 import Standard4LayersUNet
+from .standard5 import Standard5LayersUNet
+
+__all__ = [
+    "Standard4LayersUNet",
+    "Standard5LayersUNet",
+]
