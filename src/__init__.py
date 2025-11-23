77a023efad2cf44090245ff1ba8b8312839fa41c
DaoChaShao <27882272@qq.com>
2025-11-23 19:02:39 +0800

    refactor: enhance module documentation and define exports in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,13 +3,35 @@
 # @Time     :   2025/11/15 00:13
 # @Author   :   Shawn
 # @Version  :   Version 0.1.0
-# @File     :   __init__.py.py
+# @File     :   __init__.py
 # @Desc     :   
 
-def main() -> None:
-    """ Main Function """
-    pass
+"""
+****************************************************************
+Machine Learning Package
+----------------------------------------------------------------
+A comprehensive ML package containing:
+- criteria: Loss functions and evaluation metrics
+- datasets: Data loading and processing
+- nets: Neural network models
+- trainers: Training loops and strategies
+- utils: Utility functions and tools
+****************************************************************
+"""
 
+__author__ = "Shawn Yu"
+__version__ = "0.1.0"
 
-if __name__ == "__main__":
-    main()
+from . import criteria
+from . import datasets
+from . import nets
+from . import trainers
+from . import utils
+
+__all__ = [
+    "criteria",
+    "datasets",
+    "nets",
+    "trainers",
+    "utils",
+]
